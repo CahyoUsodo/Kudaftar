995134706eefe2341ae66c3842aec1cedcab0b33
Mosad Cahyo Usodo <89054080+CahyoUsodo@users.noreply.github.com>
2024-10-04 19:56:11 +0700

    Add files via upload

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'username'     => 'kudaftar_user',
+        'password'     => 'yukdaftar2025',
+        'database'     => 'kudaftar_db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
